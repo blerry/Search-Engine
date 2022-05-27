b88d9ca30564f91d4391fa3bce58c4594911122c
Larry James <larryjdel@gmail.com>
2022-05-26 22:06:54 -0700

    positional index started

--- demo/src/main/java/cecs429/indexes/Posting.java
+++ demo/src/main/java/cecs429/indexes/Posting.java
@@ -1,16 +1,27 @@
 package cecs429.indexes;
 
+import java.util.ArrayList;
+
 /**
  * A Posting encapulates a document ID associated with a search query component.
  */
-public class Posting {
+public class Posting { //Posting consists of document if with a list of positions
 	private int mDocumentId;
+	private ArrayList<Integer> mPositions; 
 	
-	public Posting(int documentId) {
+	public Posting(int documentId, ArrayList<Integer> position) {
 		mDocumentId = documentId;
+		mPositions = position;
 	}
 	
 	public int getDocumentId() {
 		return mDocumentId;
+	}
+	public ArrayList<Integer> getPostions(){ //return positions of Posting
+		return mPositions;
+	}
+	public void addPostion(int position){ //add a position to positions to the posting, sort
+		mPositions.add(position);
+		Collections.sort(mPositions);
 	}
 }
