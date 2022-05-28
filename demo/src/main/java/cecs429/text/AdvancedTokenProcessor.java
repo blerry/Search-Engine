ed8375f00080841cd949f0584b79ff1850a9e5fe
Larry James <larryjdel@gmail.com>
2022-05-27 20:55:46 -0700

    change interface

--- demo/src/main/java/cecs429/text/AdvancedTokenProcessor.java
+++ demo/src/main/java/cecs429/text/AdvancedTokenProcessor.java
@@ -1,5 +1,12 @@
 package cecs429.text;
 
+import java.util.ArrayList;
+
 public class AdvancedTokenProcessor implements TokenProcessor{
-    
+    @Override
+    public ArrayList<String> processToken(String token){
+        ArrayList<String> list = new ArrayList<String>();
+        token = token.replaceAll("^\\W+|\\W+$", ""); //start-end delete non-alphanum characters
+        return list;
+    }
 }
