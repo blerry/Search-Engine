da86796ad791efa72014a0ffb14fdb113c486f9a
Larry James <larryjdel@gmail.com>
2022-05-29 21:11:06 -0700

    phrase literal progress

--- demo/src/main/java/cecs429/queries/PhraseLiteral.java
+++ demo/src/main/java/cecs429/queries/PhraseLiteral.java
@@ -30,7 +30,7 @@ public class PhraseLiteral implements QueryComponent {
 	
 	/**
 	 * Constructs a PhraseLiteral given a string with one or more individual terms separated by spaces.
-	 
+	 /
 	public PhraseLiteral(String terms) {
 		mTerms.addAll(Arrays.asList(terms.split(" ")));
 	}
@@ -38,9 +38,76 @@ public class PhraseLiteral implements QueryComponent {
 	
 	@Override
 	public List<Posting> getPostings(Index index) {
-		return null;
-		// TODO: program this method. Retrieve the postings for the individual terms in the phrase,
+		//Retrieve the postings for the individual terms in the phrase,
 		// and positional merge them together.
+		List<Posting> pList = new ArrayList<Posting>();
+		pList = mTerms.get(0).getPostings(index); //plist is 1st element
+		
+		List<Posting> currList = new ArrayList<Posting>();//only for current Postings
+
+		//For loop + while + while loop
+		for(int i = 1; i<mTerms.size(); i++){
+			//using a and b as pointers for matching literals
+			List<Posting> tempList = new ArrayList<Posting>();
+			//currList will be used to find a match with other term
+			currList = mTerms.get(i).getPostings(index);//Find postings for current index
+			int a = 0, b = 0;//counts
+			Posting pA = pList.get(a); 
+			Posting pB = currList.get(b);
+			int docA = pA.getDocumentId();
+			int docB =  pB.getDocumentId();
+
+			while(a< pList.size() && b <currList.size()){//Loop through both lists
+				//We are looking for a match by moving both a and b
+				pA = pList.get(a);
+				pB = currList.get(b);
+				docA = pA.getDocumentId();
+				docB = pB.getDocumentId();
+
+				if(docA == docB){//iterate postings if match
+					int p1 = 0; //position 1
+					int p2 = 0; // 2
+					//Find positions
+					ArrayList<Integer> posA = pA.getPostions();
+					ArrayList<Integer> posB = pB.getPostions();
+					Posting lastPosting = null;
+
+					while(p1 < posA.size() && p2 < posB.size()){
+						if(!(tempList.isEmpty())){ //base case
+							lastPosting = tempList.get(tempList.size()-1);
+						}
+						if(posB.get(p2) == posA.get(p1)+i){
+							//if Position B = Position A + i, Then there are many
+							//So we add Posting A to TempList Postings
+							if(tempList.isEmpty())
+								tempList.add(pA);
+							else if(lastPosting.getDocumentId()==docA)//if already in tempList we add the position of Doc Id
+								lastPosting.addPosition(a);
+							else 
+								tempList.add(pA);//otherwise add Posting A
+								p1++;//Posting A position incremented
+								p2++;//Posting B
+						}
+						//Increase the min of the 2 positions if not equal
+						else if(posA.get(p1) <= posB.get(p2)){
+							p1++;
+						}
+						else{
+							p2++;
+						}
+					}
+					a++;//continue the counting
+					b++;
+				}//end match check (if)
+				//If not match increment minimum of counts
+				else{
+					if(docA<=docB) a++;
+					else b++;
+				}
+			}//end loop
+			pList =tempList; //update the list for phrase for search
+		}
+		return pList;
 	}
 	
 	@Override
