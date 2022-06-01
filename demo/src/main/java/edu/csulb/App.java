fa4ac9b91d911cf2d040d5f143b81ddf03dd71fb
Larry James <larryjdel@gmail.com>
2022-05-31 21:14:08 -0700

    small UI tests

--- demo/src/main/java/edu/csulb/App.java
+++ demo/src/main/java/edu/csulb/App.java
@@ -29,8 +29,8 @@ public class App
 
     public static void main( String[] args )
     {
-        System.out.println("http://localhost:4000/");
         Spark.staticFileLocation("public_html");
+        System.out.println("http://localhost:4567/");
         Spark.get("/", (req, res) -> {
             HashMap<String, Object> model =  new HashMap<>();
             return new ThymeleafTemplateEngine().render(new ModelAndView(model, "index"));
@@ -46,6 +46,38 @@ public class App
             //index = indexer.in
             double buildTime = 0.0;
             return "<div style=\"font-size: 12px; position:\">Files Indexed From: " + dir + " </br>Time to Index: " + buildTime +  " seconds</div></br>";
+        });
+
+
+        Spark.post("/squery", (request, response) -> {
+            String squeryValue = request.queryParams("squeryValue");
+            String stemmedTerm="";
+            if (squeryValue.length() == 2 && squeryValue.substring(1, 2).equals("q")) {
+                System.out.println("\nEnding program...");
+                System.exit(-1);
+                return "";
+            } else if (squeryValue.length() >= 5 && squeryValue.substring(1, 5).equals("stem")) {
+                //stemmedTerm = indexer.userSQueryStem(squeryValue);
+                System.out.printf("%s stemmed to: %s", squeryValue.substring(6), stemmedTerm);
+                System.out.println();
+                return "</br><div style=\"font-size: 12px;\">"+ squeryValue + " stemmed to: " + stemmedTerm + "</div></br>";
+                //build a new index from the given directory
+            } else if (squeryValue.length() >= 6 && squeryValue.substring(1, 6).equals("index")) {
+                System.out.println("Resetting the directory...");//re-build an in-memory index
+                dir = squeryValue.substring(7);
+                double buildTime = 0.0; 
+                return "<div style=\"font-size: 12px\">New Files Indexed From: " + dir + "</div> </br> <div style=\"font-size: 10px\">Time to Index:"+ buildTime +  " seconds</div>";
+                //print the first 1000 terms in the vocabulary
+            } else if (squeryValue.length() == 6 && squeryValue.substring(1, 6).equals("vocab")) {
+                List<String> vocabList = index.getVocabulary();//gather vocab list from any index
+                List<String> subVocab = null;
+                if (vocabList.size() >= 1000) { subVocab = vocabList.subList(0, 999); }
+                else { subVocab = vocabList.subList(0, vocabList.size() - 1); }
+                String formattedVocabList = subVocab.stream().map(item -> "" + item + "</br>").collect(joining(" "));
+                return "<b style=\"font-size: 15px;\"># of vocab terms: " + vocabList.size() + "</b></div></br>" + " </br> <div style=\"font-size: 12px;\">"+ formattedVocabList + "</br>";
+            } else {
+                return "<div style=\"font-size: 12px;\">Not Valid Special Query</div></br>";
+            }
         });
     }
 }
