dc32d502d8f5643acb5798d8d07cecb855d99cf0
Larry James <larryjdel@gmail.com>
2022-06-24 23:56:07 -0700

    documentation test

--- demo/src/main/java/cecs429/indexes/Index.java
+++ demo/src/main/java/cecs429/indexes/Index.java
@@ -9,17 +9,28 @@ import java.util.List;
 public interface Index {
 	/**
 	 * Retrieves a list of Postings of documents that contain the given term.
+	 * Without positions, will be required for rank
 	 */
 	List<Posting> getPostings(String term);
+	/**
+	 * A (sorted) list of all terms in the index vocabulary
+	 */
 	List<Posting> getPostingsPositions(String term);
+	/**
+	 * A (sorted) list of all terms in the index vocabulary with positions.
+	 * Will be required for a boolean phrase
+	 */
+	List<String> getVocabulary();
 	/**
 	 * A (sorted) list of all terms in the index vocabulary.
 	 */
-	List<String> getVocabulary();
-
 	int getTF(String term);
-
+	/**
+	 * Get Term frequency a term in disk.
+	 */
 	double getLD(int docId);
-
+	/**
+	 * Get the length of the document accoridng to rank retrieval
+	 */
 	int getDF_T(String term);
 }
